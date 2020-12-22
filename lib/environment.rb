f7a0663ab4a1b4550278a3f4da724fcb607d8c51
David Rafe <david.rafe@gmail.com>
2020-12-22 18:17:32 -0500

    set up skeleton CLI class with basic call method

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,6 @@
 require_relative "./sneaker_store_cli/version"
+require_relative "./sneaker_store_cli/cli.rb"
+require_relative "./sneaker_store_cli/sneaker.rb" 
 
 require "bundler"
 Bundler.require 
---
+++ lib/sneaker_store_cli/cli.rb
@@ -0,0 +1,5 @@
+class CLI 
+    def call
+       puts "Hello world!"
+    end
+end

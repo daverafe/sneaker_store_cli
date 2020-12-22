c2a6a10679cf0260ad9393d9b7b587323acf42e4
David Rafe <david.rafe@gmail.com>
2020-12-22 18:26:24 -0500

    built out call method and created a greeting method called inside the call method.

--- lib/sneaker_store_cli/cli.rb
+++ lib/sneaker_store_cli/cli.rb
@@ -1,5 +1,13 @@
 class CLI 
     def call
-       puts "Hello world!"
+       puts "Welcome to Dave's Sneaker Store!"
+       puts "What's your name?"
+       input = gets.strip
+       greeting(input)
+    end
+
+    def greeting(name)
+        input = name
+        puts "Hey #{input}! Let's find some cool sneakers!"
     end
 end

e6a39ef17b79dfc96d1eea140e8337883d61e607
David Rafe <david.rafe@gmail.com>
2020-12-22 18:57:42 -0500

    build out menu method functionality in cli class

--- lib/sneaker_store_cli/cli.rb
+++ lib/sneaker_store_cli/cli.rb
@@ -13,12 +13,29 @@ class CLI
     end
 
     def list_sneakers
+        #numbered list of all sneakers by name
+    end
 
+    def sneaker_details(name)
+        #finds sneaker by name and returns all details of that particular sneaker
     end
 
     def menu
-        puts "Take a look at what we have, then enter the number of the sneaker you want more info on."
-        list_sneakers
-        input = gets.strip.to_i - 1 
+        input = nil 
+        while input != "exit"
+            puts "Take a look at what we have, then enter the number of the sneaker you want more info on."
+            puts "To look at more sneakers, type 'list sneakers'."
+            puts "If you're done, type 'exit'."
+            list_sneakers
+            input = gets.strip.to_i - 1 
+       
+            if input > 0
+                sneaker_details(input.name)
+            elsif input == "list sneakers"
+                list_sneakers
+            else
+                puts "Not sure what that means, please enter the number of the sneaker you want, list sneakers, or exit"
+            end
+        end
     end
 end
